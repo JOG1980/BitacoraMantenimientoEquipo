82b6f2f0c7bcf104178190a8095dd013b10a5572
Jorge Ortiz Garcia <jorge.ortizga@dt.cfe.mx>
2022-10-19 03:25:08 -0500

    Se agregan componentes al index.blade de Equipo

--- resources/views/equipos/index.blade.php
+++ resources/views/equipos/index.blade.php
@@ -8,8 +8,17 @@
 @section('title','Lista de Equipos')
 
 @section('content')
-<h1>Lista de Equipos</h1>
-
+<div>
+  <div>
+    <h1>Lista de Equipos</h1>
+  </div>
+  <div>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#ModalAgregarEquipo">
+      Agregar
+    </button>
+  </div>
+</div>
 <table  class="table table-hover">
   <thead>
     <tr>
@@ -20,6 +29,7 @@
       <th scope="col">S.E.</th>
       <th scope="col">Marca</th>
       <th scope="col">Modelo</th>
+      <th scope="col">Ordenes</th>
       <th scope="col">Est. Eqp.</th>
       <th scope="col">Est. Reg.</th>
     </tr>
@@ -35,8 +45,16 @@
             <td>{{$equipo->id_subestacion}}</td>
             <td>{{$equipo->id_marca}}</td>
             <td>{{$equipo->id_modelo}}</td>
+            <td>&nbsp;</td>
             <td>{{$equipo->id_estado_equipo}}</td>
             <td>{{$equipo->id_estado_registro}}</td>
+            <td>
+              <div class="btn-group" role="group" aria-label="Opciones Registro">
+                <button type="button" class="btn btn-warning">Editar</button>
+                <button type="button" class="btn btn-primary">Duplicar</button>
+                <button type="button" class="btn btn-danger">Eliminar</button>
+              </div>
+            </td>
           </tr>
           @php($cont++)
       @endforeach
@@ -46,3 +64,40 @@
 
 {{$equipos->links()}}
 @endsection
+
+<!-- Modal Agregar-->
+<div class="modal fade" id="ModalAgregarEquipo" tabindex="-1" aria-labelledby="ModalAgregarEquipoLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="ModalAgregarEquipoLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Guardar</button>
+      </div>
+    </div>
+  </div>
+</div>
