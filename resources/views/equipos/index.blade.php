04b1e1df6ef249ca6368d2ffb043fa94a9606467
Jorge Ortiz Garcia <jorge.ortizga@dt.cfe.mx>
2022-10-19 02:40:08 -0500

    Se quita encriptacion del password del registro (RegisterController). Se agrega template al index.balde de equipos

--- resources/views/equipos/index.blade.php
+++ resources/views/equipos/index.blade.php
@@ -3,19 +3,46 @@
     
 --}}
 
-@extends('layouts.plantilla')
+{{--@extends('layouts.plantilla')--}}
+@extends('layouts.app')'
 @section('title','Lista de Equipos')
 
 @section('content')
-<h1>Hola mundo!!!</h1>
+<h1>Lista de Equipos</h1>
 
-<h1>Mostrando en la lista de equipos</h1>
+<table  class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">R3</th>
+      <th scope="col">Descripción</th>
+      <th scope="col">No Serie</th>
+      <th scope="col">S.E.</th>
+      <th scope="col">Marca</th>
+      <th scope="col">Modelo</th>
+      <th scope="col">Est. Eqp.</th>
+      <th scope="col">Est. Reg.</th>
+    </tr>
+  </thead>
+  <tbody>
+    @php($cont = 1)
+      @foreach ($equipos as $equipo)
+          <tr>
+            <th scope="row">{{$cont}}</th>
+            <td>{{$equipo->r3}}</td>
+            <td>{{$equipo->descripcion}}</td>
+            <td>{{$equipo->numero_serie}}</td>
+            <td>{{$equipo->id_subestacion}}</td>
+            <td>{{$equipo->id_marca}}</td>
+            <td>{{$equipo->id_modelo}}</td>
+            <td>{{$equipo->id_estado_equipo}}</td>
+            <td>{{$equipo->id_estado_registro}}</td>
+          </tr>
+          @php($cont++)
+      @endforeach
+  </tbody>
+</table>
 
-<ul>
-    @foreach ($equipos as $equipo)
-        <li>{{$equipo}}</li>
-    @endforeach
-</ul>
 
 {{$equipos->links()}}
 @endsection
